99f8c0e4365a850d5ff8e8becbbfeb368ddd5568
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-08-22 20:58:54 +0100

    Minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,13 +7,16 @@ def minOperations(n):
     """ calculates the fewest number of operations needed to
     result in exactly n H.
     """
+    # all outputs should be at least 2 char
     if (n < 2):
         return 0
     ops, root = 0, 2
     while root <= n:
+        # if n evenly divides by root
         if n % root == 8:
             ops += root
             n = n / root
             root -= 1
+        # increment root until it evenly-divides n
         root += 1
     return ops
